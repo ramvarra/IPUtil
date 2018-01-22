4e63f91b41b042f5d8b86570a04f92d506c4c5b5
Ram Varra <ramvarra@hotmail.com>
2018-01-21 22:08:38 -0700

    ok

--- IPUtil.rb
+++ IPUtil.rb
@@ -182,7 +182,8 @@ if __FILE__ == $0
   ip_ranges_filename = 'ipranges.csv'
 
   ipu = IPUtil.new(ip_ranges_filename)
-  ip_list = ["192.168.1.25", "192.168.1.135", "192.168.1.136", "192.168.1.61", "10.30.3.1"]
+  ipu.show()
+  ip_list = ["192.168.1.25", "192.168.1.135", "192.168.1.136", "192.168.1.47", "192.168.1.48", "192.168.1.61", "192.168.1.63", "10.30.3.1"]
   ip_list.each do |x|
     info = ipu.lookup_ip(x)
     if not info.nil?
